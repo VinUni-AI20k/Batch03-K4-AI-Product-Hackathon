be399b0130b0977df3e415e15b007a0421efb188
peterparkervanphuc <201854286+peterparkervanphuc@users.noreply.github.com>
2026-07-30 17:00:32 +0700

    fix

--- backend/test/index.spec.ts
+++ backend/test/index.spec.ts
@@ -5,17 +5,15 @@ import {
     SELF,
 } from "cloudflare:test";
 import { describe, it, expect } from "vitest";
-import worker from "../src";
+import worker, { type Env } from "../src";
 
 describe("Hello World user worker", () => {
     describe("request for /message", () => {
         it('/ responds with "Hello, World!" (unit style)', async () => {
-            const request = new Request<unknown, IncomingRequestCfProperties>(
-                "http://example.com/message"
-            );
+            const request = new Request("http://example.com/message");
             // Create an empty context to pass to `worker.fetch()`.
             const ctx = createExecutionContext();
-            const response = await worker.fetch(request, env, ctx);
+            const response = await worker.fetch(request, env as unknown as Env, ctx);
             // Wait for all `Promise`s passed to `ctx.waitUntil()` to settle before running test assertions
             await waitOnExecutionContext(ctx);
             expect(await response.text()).toMatchInlineSnapshot(`"Hello, World!"`);
@@ -30,12 +28,10 @@ describe("Hello World user worker", () => {
 
     describe("request for /random", () => {
         it("/ responds with a random UUID (unit style)", async () => {
-            const request = new Request<unknown, IncomingRequestCfProperties>(
-                "http://example.com/random"
-            );
+            const request = new Request("http://example.com/random");
             // Create an empty context to pass to `worker.fetch()`.
             const ctx = createExecutionContext();
-            const response = await worker.fetch(request, env, ctx);
+            const response = await worker.fetch(request, env as unknown as Env, ctx);
             // Wait for all `Promise`s passed to `ctx.waitUntil()` to settle before running test assertions
             await waitOnExecutionContext(ctx);
             expect(await response.text()).toMatch(
